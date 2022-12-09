ef9d810962a0c56c4dee247fdfc9621a65e242dc
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2022-12-09 11:50:53 +0200

    update begining

--- WebSite/main.go
+++ WebSite/main.go
@@ -1,4 +1,4 @@
-// 	http://localhost:8080/
+//  http://127.0.0.1:4000
 //
 //	WebSiteGo
 // 	main.go
@@ -13,17 +13,28 @@ import (
 	"net/http"
 )
 
+// Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
+// текст "Привет из Snippetbox" как тело ответа.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
 func sayhello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Привет!")
 }
 
 func main() {
+	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
+	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 
-	http.HandleFunc("/", sayhello)           // Устанавливаем роутер
-	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-
-	fmt.Println("Hello, World!")
+	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
+	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
+	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
+	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
+	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
