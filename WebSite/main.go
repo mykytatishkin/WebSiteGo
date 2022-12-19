1db8e7067acc39e1fa9afc8e0ae2ee9a3de463a9
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2022-12-19 15:05:17 +0200

    pages update

--- WebSite/main.go
+++ WebSite/main.go
@@ -1,19 +1,9 @@
-//  http://127.0.0.1:4000
-//
-//	WebSiteGo
-// 	main.go
-//
-//	Created by Mykyta Tishkin on 09.12.2022
-
 package main
 
 import (
 	"log"
 	"net/http"
 )
-
-// Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
-// текст "Привет из Snippetbox" как тело ответа.
 
 // Обработчик главной странице.
 func home(w http.ResponseWriter, r *http.Request) {
@@ -36,22 +26,33 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 
 // Обработчик для создания новой заметки.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Форма для новой заметки..."))
+	// Используем r.Method для проверки, использует ли запрос метод POST или нет. Обратите внимание,
+	// что http.MethodPost является строкой и содержит текст "POST".
+	if r.Method != http.MethodPost {
+		// Используем метод Header().Set() для добавления заголовка 'Allow: POST' в
+		// карту HTTP-заголовков. Первый параметр - название заголовка, а
+		// второй параметр - значение заголовка.
+		w.Header().Set("Allow", http.MethodPost)
+
+		// Используем функцию http.Error() для отправки кода состояния 405 с соответствующим сообщением.
+		http.Error(w, "Метод запрещен!", 405)
+
+		// Затем мы завершаем работу функции вызвав "return", чтобы
+		// последующий код не выполнялся.
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
 }
 
 func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// маршрутизаторе servemux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	// Используется функция http.ListenAndServe() для запуска нового веб-сервера.
-	// Мы передаем два параметра: TCP-адрес сети для прослушивания (в данном случае это "localhost:4000")
-	// и созданный рутер. Если вызов http.ListenAndServe() возвращает ошибку
-	// мы используем функцию log.Fatal() для логирования ошибок. Обратите внимание
-	// что любая ошибка, возвращаемая от http.ListenAndServe(), всегда non-nil.
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
